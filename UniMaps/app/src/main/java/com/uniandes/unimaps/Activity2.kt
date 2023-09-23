348fca44bd395631b724a4ad2519205881a320d2
JaimeAlfonsoR <j.alfonsor@uniandes.edu.co>
2023-09-23 04:43:20 -0500

    Vistas Walking points y feed.final

---
+++ UniMaps/app/src/main/java/com/uniandes/unimaps/Activity2.kt
@@ -0,0 +1,11 @@
+package com.uniandes.unimaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_wp)
+    }
+}
