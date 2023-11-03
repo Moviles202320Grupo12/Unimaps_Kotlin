ce4f5306066c798dacc31648ccbfa0a4cbcb6a7d
JaimeAlfonsoR <j.alfonsor@uniandes.edu.co>
2023-11-03 15:49:37 -0500

    Vistas Walking points

--- UniMaps/app/src/main/java/com/uniandes/unimaps/ui/Feed/FeedFragment.kt
+++ UniMaps/app/src/main/java/com/uniandes/unimaps/ui/Feed/FeedFragment.kt
@@ -1,16 +1,21 @@
 package com.uniandes.unimaps.ui.Feed
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.uniandes.unimaps.ui.Events.EventsFeedActivity
 import com.uniandes.unimaps.R
+import com.uniandes.unimaps.RegisterActivity
 import com.uniandes.unimaps.databinding.FragmentFeedBinding
 import com.uniandes.unimaps.ui.WalkingPoints.WalkingFragment
 import com.uniandes.unimaps.ui.home.HomeFragment
@@ -18,6 +23,7 @@ import com.uniandes.unimaps.ui.home.HomeFragment
 class FeedFragment : Fragment() {
 
     private var _binding: FragmentFeedBinding? = null
+    private val startTime = SystemClock.elapsedRealtime()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -57,16 +63,22 @@ class FeedFragment : Fragment() {
         val walkingPointsButton = root.findViewById<Button>(R.id.button8)
 
         walkingPointsButton.setOnClickListener {
-            val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.arribaFeed, WalkingFragment())
-            transaction?.disallowAddToBackStack()
-            transaction?.commit()
+            val intent = Intent (getActivity(), WalkingFragment::class.java)
+            getActivity()?.startActivity(intent)
 
         }
         return root
     }
 
     override fun onDestroyView() {
+        // Obtenemos el tiempo actual en milisegundos.
+        val endTime = SystemClock.elapsedRealtime()
+
+        // Calculamos el tiempo que el usuario pasó en la aplicación.
+        val timeInMillis = endTime - startTime
+
+        // Imprimimos el tiempo en milisegundos.
+        Log.i("Tiempo en la aplicación", timeInMillis.toString()+ " ms")
         super.onDestroyView()
         _binding = null
     }
