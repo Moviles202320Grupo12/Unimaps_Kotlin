3ad1894afaafcac180917068480ec080a16cfadb
jmRiveral <60269766+jmRiveral@users.noreply.github.com>
2023-11-04 18:56:29 -0500

    Fix de event feed

--- UniMaps/app/src/main/java/com/uniandes/unimaps/ui/Events/EventAdapter.kt
+++ UniMaps/app/src/main/java/com/uniandes/unimaps/ui/Events/EventAdapter.kt
@@ -1,11 +1,14 @@
 package com.uniandes.unimaps.ui.Events
 import android.content.Context
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.uniandes.unimaps.R
+import java.text.ParseException
+import java.util.Date
 
 class EventAdapter(
     context: Context,
@@ -30,7 +33,22 @@ class EventAdapter(
 
         eventNameTextView.text = currentEvent?.name
         eventDescriptionTextView.text = currentEvent?.description
-        eventDateTextView.text = currentEvent?.date.toString()
+
+        // Check if the date is not null before formatting
+        val date = currentEvent?.date
+        if (date != null) {
+            // Convert the Timestamp to a Date object
+            val dateObj = Date(date.seconds * 1000) // Convert seconds to milliseconds
+
+            // Format the date and time in "day/month/year hour:minute" format
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
+            val formattedDate = dateFormat.format(dateObj)
+            eventDateTextView.text = formattedDate
+        } else {
+            // Handle the case when the date is null, e.g., set text to "Date not available"
+            eventDateTextView.text = "Date not available"
+        }
+
         eventLocationTextView.text = currentEvent?.location
 
         return listItemView
